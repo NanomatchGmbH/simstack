8d6094ca358b6e6114de712304b291253979aa24
Wolfgang Wenzel <wolfgang.wenzel@kit.edu>
2015-05-22 12:00:28 +0200

    new tabs for scripts

--- WaNoWidgets.py
+++ WaNoWidgets.py
@@ -4,6 +4,22 @@
 import PySide.QtCore as QtCore
 import PySide.QtGui  as QtGui
 
+import logging
+
+#class WaNoElementWidgetFactory:
+    #factories = {}
+    #def addFactory(id, shapeFactory):
+        #WaNoElementWidgetFactory.factories.put[id] = shapeFactory
+    #addFactory = staticmethod(addFactory)
+    ## A Template Method:
+    #def createWaNoE(id,inputData):
+        #if not WaNoElementWidgetFactory.factories.has_key(id):    
+            #WaNoElementWidgetFactory.factories[id] = \
+              #eval(id + '.Factory()')
+        #return  WaNoElementWidgetFactory.factories[id].create(inputData)
+    #createWaNoEWidget = staticmethod(createWaNoEWidget)
+    
+
 def getWidgetText(myWidget):
     return myWidget.text()
     
@@ -20,7 +36,7 @@ def WaNoSectionWidget(self,ll,importSelection):
     ll = max( [ len(element.name) for element in self.elements] )
     
     for element in self.elements:
-        widget = element.widget(ll,importSelection)
+        widget = element.makeWidget(ll,importSelection)
         self.layout.addWidget(widget)
             
     self.myWidget = QtGui.QWidget()
@@ -42,6 +58,7 @@ def WaNoVarSectionWidget(self,ll,importSelection):
 
 
 class WaNoItemWidget(QtGui.QDialog):
+    sig = QtCore.Signal()
     #----------------------------------------------------------------------
     def __init__(self,waNoElement,labelSize,importSelection,parent=None):
         super(WaNoItemWidget,self).__init__(parent)
@@ -105,7 +122,17 @@ class WaNoItemWidget(QtGui.QDialog):
         
         self.setLayout(self.layout) 
     
+        self.editor.textChanged.connect(self.changed)
+        self.hiddenButton.clicked.connect(self.changed)
+        from WaNoEditor import WaNoEditor
+        self.sig.connect(WaNoEditor.hasChanged)
+       
+        
+    def changed(self):
+        self.sig.emit()
+        
     def modifyInput(self):
+        self.sig.emit()
         if len(self.importSelection) > 0:
             sel = self.typeSelect.currentText()
             if sel == 'W' or sel == 'X':
@@ -113,29 +140,58 @@ class WaNoItemWidget(QtGui.QDialog):
             else:
                 self.importSelect.hide()
     
-def makeSimpleWidget(self,ll,importSelection):    
-    self.myWidget  =  WaNoItemWidget(self,ll,importSelection,None)
-    return self.myWidget
+#def makeSimpleWidget(self,ll,importSelection):    
+    #self.myWidget  =  WaNoItemWidget(self,ll,importSelection,None)
+    #return self.myWidget
 
+class WaNoSelectionWidget(QtGui.QDialog):
+    sig = QtCore.Signal()
+    #----------------------------------------------------------------------
+    def __init__(self,waNoElement,labelSize, importSelection,parent=None):
+        super(WaNoSelectionWidget,self).__init__(parent)
+        self.layout = QtGui.QHBoxLayout(self)
+        self.logger = logging.getLogger("WEFLOG")
+        
+        # left top right bottom
+        self.layout.setContentsMargins(5,1,1,1)
+        label = QtGui.QLabel(waNoElement.name)
+        self.layout.addWidget(label)
+        label.setFixedWidth(6*labelSize+10)
+        
+        self.editor = QtGui.QListWidget()
+      
+        for i in waNoElement.elements:
+            self.editor.addItem(i.value) 
 
-def WaNoSelectionWidget(self,ll,importSelection):
-    self.layout = QtGui.QHBoxLayout()
-    # left top right bottom
-    self.layout.setContentsMargins(5,1,1,1)
-    self.layout.addWidget(QtGui.QLabel(self.name))
-    self.editor = QtGui.QListWidget()
-  
-    for i in self.elements:
-        self.editor.addItem(i.value) 
-    
-    for i in self.selectedItems:
-        self.editor.setCurrentRow(i,QtGui.QItemSelectionModel.Select)
-    self.layout.addWidget(self.editor)
-    self.hiddenbutton = QtGui.QRadioButton('hide')
-    self.layout.addWidget(self.hiddenbutton)
-    self.myWidget = QtGui.QWidget()
-    self.myWidget.setLayout(self.layout)
-    return self.myWidget  
+        if len(waNoElement.elements) == 0:
+            self.logger.error("Selection in WaNo Element " + waNoElement.name + " is empty")
+            
+        if len(waNoElement.selectedItems) == 0:
+            self.logger.error("No default item defined for WaNo Element " + waNoElement.name)
+            if len(waNoElement.elements) > 0:
+                self.editor.setCurrentRow(0,QtGui.QItemSelectionModel.Select)
+                
+        for i in waNoElement.selectedItems:
+            try:
+                self.editor.setCurrentRow(i,QtGui.QItemSelectionModel.Select)
+            except:
+                self.logger.error("Selection Failure WaNo Element " + waNoElement.name)
+                
+        self.layout.addWidget(self.editor)
+        
+        self.hiddenButton = QtGui.QRadioButton('hide')
+        self.layout.addWidget(self.hiddenButton)
+        
+        self.setLayout(self.layout)
+        
+        self.editor.itemClicked.connect(self.changed)
+        self.hiddenButton.clicked.connect(self.changed)
+        from WaNoEditor import WaNoEditor
+        self.sig.connect(WaNoEditor.hasChanged)    
+        
+    def changed(self):
+        self.sig.emit()
+
 
 def WaNoSelectionElementWidget(self,ll,importSelection):
     pass
@@ -151,25 +207,27 @@ def WaNoLocalFileWidget(self,ll,importSelection):
     widget.myValidator = QtGui.QRegExpValidator(rx, widget.editor)
     return widget
 
-def WaNoFileWidget(self,ll,importSelection):
-    widget = makeSimpleWidget(self,ll,importSelection) 
-    rx = QtCore.QRegExp("\\S+")
-    widget.myValidator = QtGui.QRegExpValidator(rx, widget.editor)
-    return widget
+class WaNoFileWidget(WaNoItemWidget):
+    def __init__(self,waNoElement,ll,importSelection):
+        super(WaNoFileWidget,self).__init__(waNoElement,ll,importSelection)
+        rx = QtCore.QRegExp("\\S+")
+        self.myValidator = QtGui.QRegExpValidator(rx, self.editor)
+      
+class WaNoOutputFileWidget(WaNoItemWidget):
+    def __init__(self,waNoElement,ll,importSelection):
+        super(WaNoOutputFileWidget,self).__init__(waNoElement,ll,importSelection)
+        rx = QtCore.QRegExp("\\S+")
+        self.myValidator = QtGui.QRegExpValidator(rx, self.editor)
+        
 
-def WaNoOutputFileWidget(self,ll,importSelection):
-    widget = makeSimpleWidget(self,ll,importSelection)
-    rx = QtCore.QRegExp("\\S+")
-    widget.myValidator = QtGui.QRegExpValidator(rx, widget.editor)
-    return self.myWidget  
+class WaNoFloatWidget(WaNoItemWidget):
+    def __init__(self,waNoElement,ll,importSelection):
+        super(WaNoFloatWidget,self).__init__(waNoElement,ll,importSelection)
+        self.myValidator = QtGui.QDoubleValidator(self.editor)
 
-def WaNoFloatWidget(self,ll,importSelection):
-    widget = makeSimpleWidget(self,ll,importSelection)
-    widget.myValidator = QtGui.QDoubleValidator(widget.editor)
-    return widget
-
-def WaNoIntWidget(self,ll,importSelection):
-    widget = makeSimpleWidget(self,ll,importSelection) 
-    widget.editor.setValidator(QtGui.QIntValidator(widget.editor))
-    return widget
+class WaNoIntWidget(WaNoItemWidget):
+    def __init__(self,waNoElement,ll,importSelection):
+        super(WaNoIntWidget,self).__init__(waNoElement,ll,importSelection)
+        self.myValidator = QtGui.QIntValidator(self.editor)
+   
     
