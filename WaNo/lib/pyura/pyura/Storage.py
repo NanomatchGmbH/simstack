b3ce8aaf03b9460c62ba7251f04dc36a4b51edf1
Florian Muenchbach <florian.muenchbach@gmail.com>
2016-05-31 15:22:54 +0200

    Updated pyura to pyura/dev:0a23294.

--- WaNo/lib/pyura/pyura/Storage.py
+++ WaNo/lib/pyura/pyura/Storage.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
-from pyura.ServerPrimitive import ServerPrimitive
+from .ServerPrimitive import ServerPrimitive
 from functools import wraps
 
 class Storage(ServerPrimitive):
