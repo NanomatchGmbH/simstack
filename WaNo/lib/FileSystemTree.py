d93adcbfdc274ec15cdc27c59872b5c21bcf1c7e
Timo Strunk <Timo.Strunk@nanomatch.com>
2016-06-22 13:31:12 +0200

    copytree helper function in FileSystemTree

--- WaNo/lib/FileSystemTree.py
+++ WaNo/lib/FileSystemTree.py
@@ -14,6 +14,16 @@ from PySide.QtCore import QAbstractItemModel, QModelIndex, QFileInfo
 from PySide.QtGui import QFileIconProvider
 import PySide.QtCore
 import abc
+import shutil
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 class NodeContainer(object):
     def __init__(self):
