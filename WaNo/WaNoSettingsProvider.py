3d78b9e8f379ce159690c01459ba0c874f23425e
Timo Strunk <Timo.Strunk@nanomatch.com>
2016-06-22 13:30:19 +0200

    Settings now include homepath-wano-workspace as default dir for workflows, singleton usage

--- WaNo/WaNoSettingsProvider.py
+++ WaNo/WaNoSettingsProvider.py
@@ -2,20 +2,33 @@ import os
 
 from .lib.AbstractSettings import AbstractSettings
 from .Constants import SETTING_KEYS
+import PySide.QtCore as QtCore
 
 def _path(name):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), name)
 
+class NotInstancedError(Exception):
+    pass
+
 class WaNoSettingsProvider(AbstractSettings):
+    instance = None
     def __init__(self, settings_file):
         super(WaNoSettingsProvider, self)\
                 .__init__("WaNoSettingsProvider", settings_file=settings_file)
+
+    @classmethod
+    def get_instance(cls, settings_file = None):
+        if cls.instance is None and settings_file is None:
+            raise NotInstancedError()
+        if cls.instance is None:
+            cls.instance = WaNoSettingsProvider(settings_file)
+        return cls.instance
 
     def _set_defaults(self):
         defaults = [
                 (SETTING_KEYS['registries'], [], "List of registries."),
                 (SETTING_KEYS['wanoRepo'], _path("WaNoRepository"), "Directory containing the WaNos."),
-                (SETTING_KEYS['workflows'], _path("WorkFlows"), "Working directory for Workflows."),
+                (SETTING_KEYS['workflows'], os.path.join(QtCore.QDir.homePath(),"wano-workspace"), "Working directory for Workflows.")
             ]
 
         for valuename,default,explanation in defaults:
