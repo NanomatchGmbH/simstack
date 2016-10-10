0ce7d1bafe16633e7290aa9b1a0ea5cab24ce5c2
Florian Muenchbach <florian.muenchbach@gmail.com>
2016-10-10 16:49:11 +0200

    Added support for lists in UnicoreState.

--- WaNo/UnicoreState.py
+++ WaNo/UnicoreState.py
@@ -1,4 +1,4 @@
-from PySide.QtCore import QReadWriteLock
+from PySide.QtCore import QReadWriteLock, QMutex
 
 class UnicoreStateValues:
     CONNECTION      = "connection"
@@ -11,6 +11,62 @@ class UnicoreStateFactory:
             self.write_lock()
             self.values[key] = value
             self.write_unlock()
+
+        """ Adds a new list.
+        In case an initial list is given, it will be added as a copy.
+        This will prevent unsave changes.
+        """
+        def add_list(self, key, initial_list):
+            rv = False
+            self.listlock.write_lock()
+            if not key in self.lists:
+                l = {
+                        'lock': QReadWriteLock(QReadWriteLock.NonRecursive),
+                        'list': initial_list.copy()
+                    }
+                self.lists[key] = l
+                rv = True
+            self.listlock.write_unlock()
+            return rv
+
+        """ Savely removes and returns a list.
+        """
+        def remove_list(self, key):
+            rv = None
+            self.listlock.write_lock()
+            if key in self.lists:
+                rv = self.lists[key]
+                del(self.lists[key])
+            self.listlock.write_unlock()
+            del(rv['lock'])
+            return rv['list']
+
+        def add_items_to_list(self, key, items):
+            rv = False
+            self.listlock.read_lock()
+            if key in self.lists:
+                lock = self.lists[key]['lock']
+                l    = self.lists[key]['list']
+                lock.write_lock()
+                l.extend([items] if len(items) <= 1 else items)
+                lock.write_unlock()
+                rv = True
+            self.listlock.read_unlock()
+            return rv
+
+        def get_list_iterator(self, key):
+            self.listlock.read_lock()
+            if key in self.lists:
+                lock = self.lists[key]['lock']
+                l    = self.lists[key]['list']
+                lock.read_lock()
+                for i in l:
+                    yield i
+                lock.read_unlock()
+            self.listlock.read_unlock()
+
+        def get_value_no_lock(self, key):
+            return self.value[key]
 
         def get_value(self, key):
             self.read_lock()
@@ -34,12 +90,20 @@ class UnicoreStateFactory:
             return self.lock
     
         def __init__(self):
-            self.lock   = QReadWriteLock(QReadWriteLock.NonRecursive)
-            self.values = {}
+            self.lock       = QReadWriteLock(QReadWriteLock.NonRecursive)
+            self.listlock   = QReadWriteLock(QReadWriteLock.NonRecursive)
+            self.values     = {}
+            self.lists      = {}
+
+    @classmethod
+    def __setup_instance(instance):
+        instance.add_list('dls')
     
     @classmethod
     def get_instance(self):
+        #TODO mutex lock this!
         if UnicoreStateFactory._instance is None:
                 UnicoreStateFactory._instance = UnicoreStateFactory.__UnicoreState()
+                UnicoreStateFactory.__setup_instance(UnicoreStateFactory._instance)
         return UnicoreStateFactory._instance
 
