32ad07145a624d709cdc9786c141ee7822803cf2
Timo Strunk <Timo.Strunk@nanomatch.com>
2016-10-11 13:18:00 +0200

    Removed pyura

--- WaNo/lib/pyura/pyura/Exceptions.py
+++
@@ -1,7 +0,0 @@
-#import Exception
-
-class DeletedResourceRequested(Exception):
-    """This exception is raised when a resource is requested that has been
-    deleted on the server but was not (yet) deleted on the client.
-    """
-    pass
--- WaNo/lib/pyura/pyura/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import UnicoreAPI
-from .HTTPBasicAuthProvider import HTTPBasicAuthProvider
-from .Constants import ErrorCodes, HTTPStatusCodes, JobStatus, ResourceStatus, Protocols
-from .Registry import Registry
-from .Storage import Storage
-#from pyura.Job import Job
-
