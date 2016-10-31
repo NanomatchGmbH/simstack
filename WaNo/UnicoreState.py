a6273a231eb33ff71917952cae346c251ba7a948
Florian Muenchbach <florian.muenchbach@gmail.com>
2016-10-31 13:23:47 +0100

    Fixed locks.

--- WaNo/UnicoreState.py
+++ WaNo/UnicoreState.py
@@ -16,9 +16,9 @@ class UnicoreStateFactory:
         In case an initial list is given, it will be added as a copy.
         This will prevent unsave changes.
         """
-        def add_list(self, key, initial_list):
+        def add_list(self, key, initial_list=[]):
             rv = False
-            self.listlock.write_lock()
+            self.listslock.lockForWrite()
             if not key in self.lists:
                 l = {
                         'lock': QReadWriteLock(QReadWriteLock.NonRecursive),
@@ -26,44 +26,44 @@ class UnicoreStateFactory:
                     }
                 self.lists[key] = l
                 rv = True
-            self.listlock.write_unlock()
+            self.listslock.unlock()
             return rv
 
         """ Savely removes and returns a list.
         """
         def remove_list(self, key):
             rv = None
-            self.listlock.write_lock()
+            self.listslock.lockForWrite()
             if key in self.lists:
                 rv = self.lists[key]
                 del(self.lists[key])
-            self.listlock.write_unlock()
+            self.listslock.unlock()
             del(rv['lock'])
             return rv['list']
 
         def add_items_to_list(self, key, items):
             rv = False
-            self.listlock.read_lock()
+            self.listslock.lockForRead()
             if key in self.lists:
                 lock = self.lists[key]['lock']
                 l    = self.lists[key]['list']
-                lock.write_lock()
+                lock.lockForWrite()
                 l.extend([items] if len(items) <= 1 else items)
-                lock.write_unlock()
+                lock.unlock()
                 rv = True
-            self.listlock.read_unlock()
+            self.listslock.unlock()
             return rv
 
         def get_list_iterator(self, key):
-            self.listlock.read_lock()
+            self.listslock.lockForRead()
             if key in self.lists:
                 lock = self.lists[key]['lock']
                 l    = self.lists[key]['list']
-                lock.read_lock()
+                lock.lockForRead()
                 for i in l:
                     yield i
-                lock.read_unlock()
-            self.listlock.read_unlock()
+                lock.unlock()
+            self.listslock.unlock()
 
         def get_value_no_lock(self, key):
             return self.value[key]
@@ -91,7 +91,7 @@ class UnicoreStateFactory:
     
         def __init__(self):
             self.lock       = QReadWriteLock(QReadWriteLock.NonRecursive)
-            self.listlock   = QReadWriteLock(QReadWriteLock.NonRecursive)
+            self.listslock  = QReadWriteLock(QReadWriteLock.NonRecursive)
             self.values     = {}
             self.lists      = {}
 
@@ -107,7 +107,7 @@ class UnicoreStateFactory:
             self._state = state
 
     @classmethod
-    def __setup_instance(instance):
+    def __setup_instance(self, instance):
         instance.add_list('dls')
     
     @classmethod
