32af0eaa6452bd626527d74e44cffa65de11ddd7
Timo Strunk <Timo.Strunk@nanomatch.com>
2016-11-29 16:58:25 +0100

    Added release script for externals

---
+++ release_tools/git_archive.sh
@@ -0,0 +1,19 @@
+if [ ! -f WaNoMain.py ]
+then
+    echo "Main executable not found, quitting."
+    exit 0
+fi
+git archive --format zip --output nanomatch-client.zip develop
+cd external/pyura 
+git archive --prefix external/pyura/ --format zip --output ../../pyura_archive.zip dev
+cd -
+
+mkdir nanomatch-client
+cd nanomatch-client
+unzip ../nanomatch-client.zip
+unzip ../pyura_archive.zip
+cd -
+rm nanomatch-client.zip pyura_archive.zip
+
+zip -r nanomatch-client.zip nanomatch-client
+rm -r nanomatch-client
