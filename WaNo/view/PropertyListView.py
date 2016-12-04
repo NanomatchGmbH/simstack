6167a6903accc433705f53373ae8d128bb96570b
Timo Strunk <Timo.Strunk@nanomatch.com>
2016-12-04 17:33:45 +0100

    First version of propertylistmodel and view, in one file still

---
+++ WaNo/view/PropertyListView.py
@@ -0,0 +1,378 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+
+from enum import Enum
+
+from PySide import QtGui, QtCore
+
+import sip
+import yaml
+
+sip.setapi('QString', 2)
+sip.setapi('QVariant', 2)
+
+from PyQt4 import QtCore, QtGui
+import operator
+
+#Andere Liste
+#mit einträgen + - widget
+#und render
+
+
+class ResourceTableModel(QtCore.QAbstractTableModel):
+    class ROWTYPE(Enum):
+        ppn = 1
+        numnodes = 2
+        mem = 3
+        time = 4
+        queue = 5
+
+    def save(self,filename):
+        with open(filename,'w') as outfile:
+            outfile.write(yaml.safe_dump(self.mylist, default_flow_style=False))
+
+    def load(self,filename):
+        with open(filename, 'r') as infile:
+            self.mylist = yaml.safe_load(infile)
+
+    def __init__(self, parent, *args):
+        super(ResourceTableModel, self).__init__(parent, *args)
+        self.mylist = [[]]
+        self.types = []
+        self.header = []
+        self.alignments = []
+
+    def make_default_list(self):
+        self.mylist = [
+            [False,"CPUs per Node",1],
+            [True, "Number of Nodes",1],
+            [False, "Memory [MB]",1024],
+            [False, "Time [Wall]", 86400],
+            [True, "Queue", "default"]
+        ]
+        self.header = ["Enable", "Property", "Value"]
+        self.alignments = [QtCore.Qt.AlignCenter, QtCore.Qt.AlignLeft| QtCore.Qt.AlignVCenter, QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter]
+
+    def flags(self, index):
+        if (index.column() == 1):
+            return QtCore.Qt.ItemIsEnabled
+        else:
+            return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled
+
+
+    def render_to_resource_jsdl(self):
+        from pyura.pyura.WorkflowXMLConverter import JSDLtoXML
+        ppn = None
+        if self.mylist[self.ROWTYPE.ppn][0] == True:
+            ppn = self.mylist[self.ROWTYPE.ppn][2]
+
+        numnodes = None
+        if self.mylist[self.ROWTYPE.numnodes][0] == True:
+            ppn = self.mylist[self.ROWTYPE.numnodes][2]
+
+        mem = None
+        if self.mylist[self.ROWTYPE.mem][0] == True:
+            ppn = self.mylist[self.ROWTYPE.mem][2]
+
+        time = None
+        if self.mylist[self.ROWTYPE.time][0] == True:
+            ppn = self.mylist[self.ROWTYPE.time][2]
+
+        queue = None
+        if self.mylist[self.ROWTYPE.queue][0] == True:
+            ppn = self.mylist[self.ROWTYPE.queue][2]
+
+        return JSDLtoXML.xml_resources(IndividualCPUCount=ppn,TotalResourceCount=numnodes,
+                                       IndividualPhysicalMemory=mem*1024*1024,
+                                       Queue=queue, IndividualCPUTime=time)
+
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        return len(self.mylist)
+
+    def columnCount(self,parent=QtCore.QModelIndex()):
+        return len(self.mylist[0])
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        elif role == QtCore.Qt.TextAlignmentRole:
+            return self.alignments[index.column()]
+        elif role != QtCore.Qt.DisplayRole:
+            return None
+        return self.mylist[index.row()][index.column()]
+
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        if role == QtCore.Qt.EditRole:
+            row = index.row()
+            col = index.column()
+            self.mylist[row][col] = value
+
+            return True
+
+        return False
+
+    def headerData(self, col, orientation, role):
+        if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
+            return self.header[col]
+        return None
+
+    def sort(self, col, order):
+        """sort table by given column number col"""
+        self.layoutAboutToBeChanged.emit()
+        self.mylist = sorted(self.mylist,
+                             key=operator.itemgetter(col))
+        if order == QtCore.Qt.DescendingOrder:
+            self.mylist.reverse()
+        self.layoutChanged.emit()
+
+
+
+class FileChooserDelegate(QtGui.QItemDelegate):
+    def __init__(self, parent):
+        QtGui.QItemDelegate.__init__(self, parent)
+
+    def paint(self, painter, option, index):
+
+        if (option.state & QtGui.QStyle.State_Selected):
+            painter.fillRect(option.rect, option.palette.highlight());
+
+
+        return
+        if index.column() == IMAGE:
+            painter.drawPixmap(option.rect,
+                               QPixmap(index.model().data(index).toString()))
+
+        #QItemDelegate.paint(self, painter, option, index)
+
+    def createEditor(self, parent, option, index):
+        custom_widget = QtGui.QWidget(parent)
+        hbl = QtGui.QHBoxLayout()
+        custom_widget.setLayout(hbl)
+
+        label = QtGui.QLabel(str(index.data()), custom_widget)
+        hbl.addWidget(label)
+        hbl.addStretch()
+        combo = QtGui.QPushButton(str(index.data()), custom_widget)
+        hbl.addWidget(combo)
+
+        #hbl.setSpacing(0)
+        hbl.setContentsMargins(0,0,0,0)
+        hbl.setAlignment(QtCore.Qt.AlignCenter)
+        hbl.setSizeConstraint(QtGui.QLayout.SetMaximumSize)
+        # self.connect(combo, QtCore.SIGNAL("currentIndexChanged(int)"), self, QtCore.SLOT("currentIndexChanged()"))
+        #combo.clicked.connect(self.currentIndexChanged)
+        return custom_widget
+
+    def setEditorData(self, editor, index):
+        editor.blockSignals(True)
+        # editor.setCurrentIndex(int(index.model().data(index)))
+        editor.blockSignals(False)
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.text())
+
+    @QtCore.pyqtSlot()
+    def currentIndexChanged(self):
+        self.commitData.emit(self.sender())
+
+class ButtonDelegate(QtGui.QItemDelegate):
+    def __init__(self, parent):
+        QtGui.QItemDelegate.__init__(self, parent)
+
+    def createEditor(self, parent, option, index):
+        combo = QtGui.QPushButton(str(index.data()), parent)
+
+        # self.connect(combo, QtCore.SIGNAL("currentIndexChanged(int)"), self, QtCore.SLOT("currentIndexChanged()"))
+        combo.clicked.connect(self.currentIndexChanged)
+        return combo
+
+    def setEditorData(self, editor, index):
+        editor.blockSignals(True)
+        # editor.setCurrentIndex(int(index.model().data(index)))
+        editor.blockSignals(False)
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.text())
+
+    @QtCore.pyqtSlot()
+    def currentIndexChanged(self):
+        self.commitData.emit(self.sender())
+
+
+
+class CheckBoxDelegate(QtGui.QStyledItemDelegate):
+    """
+    A delegate that places a fully functioning QCheckBox in every
+    cell of the column to which it's applied
+    """
+    def __init__(self, parent):
+        QtGui.QItemDelegate.__init__(self, parent)
+
+    def createEditor(self, parent, option, index):
+        '''
+        Important, otherwise an editor is created if the user clicks in this cell.
+        ** Need to hook up a signal to the model
+        '''
+        return None
+
+    def paint(self, painter, option, index):
+        '''
+        Paint a checkbox without the label.
+        '''
+
+        checked = index.data()
+        check_box_style_option = QtGui.QStyleOptionButton()
+
+        if (index.flags() & QtCore.Qt.ItemIsEditable):
+            check_box_style_option.state |= QtGui.QStyle.State_Enabled
+        else:
+            check_box_style_option.state |= QtGui.QStyle.State_ReadOnly
+
+        if checked:
+            check_box_style_option.state |= QtGui.QStyle.State_On
+        else:
+            check_box_style_option.state |= QtGui.QStyle.State_Off
+
+        check_box_style_option.rect = self.getCheckBoxRect(option)
+
+        # this will not run - hasFlag does not exist
+        #if not index.model().hasFlag(index, QtCore.Qt.ItemIsEditable):
+            #check_box_style_option.state |= QtGui.QStyle.State_ReadOnly
+
+        check_box_style_option.state |= QtGui.QStyle.State_Enabled
+
+        QtGui.QApplication.style().drawControl(QtGui.QStyle.CE_CheckBox, check_box_style_option, painter)
+
+    def editorEvent(self, event, model, option, index):
+        '''
+        Change the data in the model and the state of the checkbox
+        if the user presses the left mousebutton or presses
+        Key_Space or Key_Select and this cell is editable. Otherwise do nothing.
+        '''
+        if not (index.flags() & QtCore.Qt.ItemIsEditable):
+            return False
+
+        # Do not change the checkbox-state
+        if event.type() == QtCore.QEvent.MouseButtonPress:
+          return False
+        if event.type() == QtCore.QEvent.MouseButtonRelease or event.type() == QtCore.QEvent.MouseButtonDblClick:
+            if event.button() != QtCore.Qt.LeftButton or not self.getCheckBoxRect(option).contains(event.pos()):
+                return False
+            if event.type() == QtCore.QEvent.MouseButtonDblClick:
+                return True
+        elif event.type() == QtCore.QEvent.KeyPress:
+            if event.key() != QtCore.Qt.Key_Space and event.key() != QtCore.Qt.Key_Select:
+                return False
+            else:
+                return False
+
+        # Change the checkbox-state
+        self.setModelData(None, model, index)
+        return True
+
+    def setModelData (self, editor, model, index):
+        '''
+        The user wanted to change the old state in the opposite.
+        '''
+        newValue = not index.data()
+        model.setData(index, newValue, QtCore.Qt.EditRole)
+
+    def getCheckBoxRect(self, option):
+        check_box_style_option = QtGui.QStyleOptionButton()
+        check_box_rect = QtGui.QApplication.style().subElementRect(QtGui.QStyle.SE_CheckBoxIndicator, check_box_style_option, None)
+        check_box_point = QtCore.QPoint (option.rect.x() +
+                            option.rect.width() / 2 -
+                            check_box_rect.width() / 2,
+                            option.rect.y() +
+                            option.rect.height() / 2 -
+                            check_box_rect.height() / 2)
+        return QtCore.QRect(check_box_point, check_box_rect.size())
+
+class ComboDelegate(QtGui.QItemDelegate):
+    """
+    A delegate that places a fully functioning QComboBox in every
+    cell of the column to which it's applied
+    """
+
+    def __init__(self, parent):
+        QtGui.QItemDelegate.__init__(self, parent)
+
+    def createEditor(self, parent, option, index):
+        combo = QtGui.QComboBox(parent)
+        li = []
+        li.append("Zero")
+        li.append("One")
+        li.append("Two")
+        li.append("Three")
+        li.append("Four")
+        li.append("Five")
+        combo.addItems(li)
+        self.connect(combo, QtCore.SIGNAL("currentIndexChanged(int)"), self, QtCore.SLOT("currentIndexChanged()"))
+        return combo
+
+    def setEditorData(self, editor, index):
+        editor.blockSignals(True)
+        editor.setCurrentIndex(int(index.model().data(index)))
+        editor.blockSignals(False)
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.currentIndex())
+
+    @QtCore.pyqtSlot()
+    def currentIndexChanged(self):
+        self.commitData.emit(self.sender())
+
+
+class TableView(QtGui.QTableView):
+    """
+    A simple table to demonstrate the QComboBox delegate.
+    """
+
+    def __init__(self, *args, **kwargs):
+        QtGui.QTableView.__init__(self, *args, **kwargs)
+
+        # Set the delegate for column 0 of our table
+        # self.setItemDelegateForColumn(0, ButtonDelegate(self))
+
+        #self.setItemDelegateForColumn(0, ComboDelegate(self))
+        self.setItemDelegateForColumn(0, CheckBoxDelegate(self))
+        #self.setItemDelegateForColumn(0, FileChooserDelegate(self))
+        #self.setItemDelegateForColumn(1, CheckBoxDelegate(self))
+
+
+
+if __name__ == "__main__":
+    from sys import argv, exit
+
+
+    class Widget(QtGui.QWidget):
+        """
+        A simple test widget to contain and own the model and table.
+        """
+
+        def __init__(self, parent=None):
+            QtGui.QWidget.__init__(self, parent)
+
+            l = QtGui.QVBoxLayout(self)
+            self._tm = ResourceTableModel(self)
+            self._tm.make_default_list()
+            self._tv = TableView(self)
+            self._tv.setModel(self._tm)
+            for row in range(0, self._tm.rowCount()):
+                self._tv.openPersistentEditor(self._tm.index(row, 0))
+
+
+            l.addWidget(self._tv)
+            self._tv.resizeColumnsToContents()
+
+
+    a = QtGui.QApplication(argv)
+    w = Widget()
+    w.show()
+    w.raise_()
+    exit(a.exec_())
