6c09b964cfbbd0a6fbdc5b5d5a20f52eff0ac158
Florian Muenchbach <florian.muenchbach@gmail.com>
2017-03-24 19:34:49 +0100

    Adapting the GUI timer dynamically to avoid unnecessary ticks.

--- WaNo/view/ViewTimer.py
+++ WaNo/view/ViewTimer.py
@@ -14,8 +14,6 @@ def write_output(text):
 
 class ContinuousViewTimer(QObject):
     def add_callback(self, callback, interval):
-        print("nrdnrd")
-        interval = math.ceil(interval / self._base_tick)
         if not interval in self._interval_list:
             self._interval_list[interval] = []
 
@@ -29,16 +27,30 @@ class ContinuousViewTimer(QObject):
 
         if not self._timer.isActive():
             print("start timer")
+            self._base_tick = interval
             self._timer.start(self._base_tick)
         else:
-            print("timerself._timer.isActive()")
-            print(self._timer.isActive())
+            # check, if the base_tick time must be shorter to satisfy new timer
+            # requirements
+            gcd = self.__recalc_base_tick(interval)
+            if gcd < self._base_tick:
+                if self._base_tick is None:
+                    self._tick_counter *= self._base_tick / gcd
+                else:
+                    self._tick_counter = 0
+                self._base_tick = gcd
+                self._timer.setInterval(self._base_tick)
+                print("setting Interval to new value: %d" % self._base_tick)
+
+    def __recalc_base_tick(self, new_value):
+        return new_value if self._base_tick is None \
+                else math.gcd(self._base_tick, new_value)
 
     def __update_max_tick(self, new_value):
         self._tick_max = least_common_multiple(self._tick_max, new_value)
         print("new max: %d" % self._tick_max)
 
-    def remove_callback(self, interval, callback):
+    def remove_callback(self, callback, interval):
         if interval in self._interval_list:
             if callback in self._interval_list[interval]:
                 self._interval_list[interval][callback] -= 1
@@ -48,29 +60,40 @@ class ContinuousViewTimer(QObject):
 
             # are there any callbacks for this interval left?
             if len(self._interval_list[interval]) == 0:
+                print("recalc")
                 del(self._interval_list[interval])
-                self._recalc_max_tick=True
+                self._recalc   = True
 
         # are there any callbacks left? if not, stop timer.
-        if len(self._interval_list):
+        if len(self._interval_list) == 0:
             self._timer.stop()
 
-
     def __timeout(self):
-        print("timeout")
         recalc = False
-        if self._recalc_max_tick and (self._tick_counter == 0):
+        if self._recalc and (self._tick_counter == 0):
             recalc = True
+            self._recalc = False
             self._tick_max = 1
+            self._base_tick = None
+            current_interval = 0
+        else:
+            current_interval = self._tick_counter * self._base_tick
 
-        for freq in self._interval_list.keys():
-            if self._tick_counter % freq == 0:
-                for callback in self._interval_list[freq]:
+        for interval in self._interval_list.keys():
+            if current_interval % interval == 0:
+                for callback in self._interval_list[interval]:
                     callback()
             if recalc:
-                self.__update_max_tick(freq)
+                self.__update_max_tick(interval)
+                self._base_tick = self.__recalc_base_tick(interval)
 
-        self._tick_counter = (self._tick_counter + 1) % self._tick_max
+        if recalc:
+            self._timer.setInterval(self._base_tick)
+            print("resetting timer to : %d" % self._base_tick)
+
+        tick_max = self._tick_max / self._base_tick
+
+        self._tick_counter = (self._tick_counter + 1) % tick_max
 
     def __init__(self, base_tick=100):
         super(ContinuousViewTimer, self).__init__()
@@ -78,13 +101,13 @@ class ContinuousViewTimer(QObject):
         self._tick_counter=0
         self._tick_max=1
         self._interval_list={}
-        self._recalc_max_tick = False
+        self._recalc = False
         ####
 
         self._timer = QTimer(self)
-        self._base_tick=base_tick# * 10
+        self._base_tick=None
         self._timer.timeout.connect(self.__timeout)
-        self._timer.start(100)
+        #self._timer.start(self._base_tick)
 
 #               #
 # - - - - - - - - - - - - -
@@ -106,6 +129,8 @@ if __name__ == "__main__":
         print("3")
 
     class APP(QCoreApplication):
+        def get_ct(self):
+            return self.ct
         def __init__(self):
             super(APP, self).__init__([])
 
@@ -113,20 +138,27 @@ if __name__ == "__main__":
             #self.timer.timeout.connect(foo)
             #self.timer.start(1000)
             self.ct = ContinuousViewTimer()
-            self.ct.add_callback(test1, 1000)
+            self.ct.add_callback(test3, 4000)
             self.ct.add_callback(test2, 2000)
-            self.ct.add_callback(test3, 3000)
             super(APP, self).quit()
 
             print("App")
     class TestThread(QThread):
         def run(self):
+            time.sleep(7)
+            print("run")
+            self.ct.add_callback(test1, 1000)
+            time.sleep(4)
+            self.ct.remove_callback(test1, 1000)
             time.sleep(10)
             print("done")
             app.quit()
+        def __init__(self, ct):
+            super(TestThread, self).__init__()
+            self.ct = ct
 
     app = APP()
-    t = TestThread()
+    t = TestThread(app.get_ct())
     t.start()
     sys.exit(app.exec_())
 
