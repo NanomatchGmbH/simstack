1cc48c36b27bfabb314c9f088040226200f7af84
Timo Strunk <Timo.Strunk@nanomatch.com>
2017-06-28 15:37:26 +0200

    git archive zips master branch now

--- release_tools/git_archive.sh
+++ release_tools/git_archive.sh
@@ -3,7 +3,7 @@ then
     echo "Main executable not found, quitting."
     exit 0
 fi
-git archive --format zip --output nanomatch-client.zip develop
+git archive --format zip --output nanomatch-client.zip master
 cd external/pyura 
 git archive --prefix external/pyura/ --format zip --output ../../pyura_archive.zip dev
 cd -
