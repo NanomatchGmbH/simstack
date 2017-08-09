c65c1de789e8431c0373c7cc3a4ce6f099c8b6d9
Timo Strunk <Timo.Strunk@nanomatch.com>
2017-08-09 16:39:38 +0200

    Added qtpy convenience getter

---
+++ external/get_qtpy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#wget "https://pypi.python.org/packages/f7/51/7dd6a8be4e5bbd0af04d854e20b23f04f4b956c1ec4d2fddda8d72690303/Qt.py-1.1.0b2.tar.gz#md5=adff90bb59fc231034b2010e5438c2e2"
+#tar xf Qt.py-1.1.0b2.tar.gz
+#mv Qt.py-1.1.0b2/Qt.py ./
+#rm -r Qt.py-1.1.0b2.tar.gz Qt.py-1.1.0b2/
+wget https://raw.githubusercontent.com/mottosso/Qt.py/master/Qt.py
