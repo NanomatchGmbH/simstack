e86986d645754205e80f1fffad85e73e3aa84bae
Timo Strunk <Timo.Strunk@nanomatch.com>
2020-11-26 11:22:49 +0100

    Re-added reload button

--- WaNo/view/WFRemoteFileSystem.py
+++ WaNo/view/WFRemoteFileSystem.py
@@ -35,10 +35,10 @@ class WFRemoteFileSystem(QWidget):
         self.__fileTree.setHeaderHidden(True)
         self.__fileTree.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.__fileTree.setContextMenuPolicy(Qt.CustomContextMenu)
-        #self.__btn_reload.setText("Reload")
+        self.__btn_reload.setText("Reload")
 
         layout.addWidget(self.__fileTree)
-        #layout.addWidget(self.__btn_reload)
+        layout.addWidget(self.__btn_reload)
 
     def update_job_list(self, jobs):
         self.update_file_tree_node(self._JOB_PATH, jobs)
