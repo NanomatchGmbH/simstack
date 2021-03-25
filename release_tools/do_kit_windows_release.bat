8da5a3c0e3340daa96ef846ae827f4e0d0663e1a
Timo Strunk <timo.strunk@nanomatch.com>
2021-03-25 13:15:22 +0100

    Windows release script now automatically uploads release

--- release_tools/do_kit_windows_release.bat
+++ release_tools/do_kit_windows_release.bat
@@ -1,3 +1,7 @@
 SET datestring=%date:~6,4%-%date:~3,2%-%date:~0,2%
-.\simstack_src\release_tools\compiled_client.bat
-rsync.exe -av %datestring%-simstack_linux.tar.gz nanodev:WordPress_SecureMode_01/nanomatch-files/software/simstack/current_release/
+
+CALL .\simstack_src\release_tools\compiled_client.bat
+
+echo "DONE BUILDING."
+rsync %datestring%-simstack_windows.zip -avzh -e "ssh -F %USERPROFILE%\.ssh\config -i %USERPROFILE%\.ssh\id_rsa" nanodev:WordPress_SecureMode_01/nanomatch-files/software/simstack/current_release/
+echo "RSYNC done"
