2079e9284574f156b683a231efea50989cdd9e03
Timo Strunk <timo.strunk@nanomatch.com>
2021-03-25 13:14:43 +0100

    Windows release script now automatically uploads release

--- release_tools/compiled_client.bat
+++ release_tools/compiled_client.bat
@@ -48,4 +48,7 @@ xcopy simstack_src\Logo.png installer_package\ /s
 ren installer_package simstack
 
 SET datestring=%date:~6,4%-%date:~3,2%-%date:~0,2%
+rd /s /q %datestring%-simstack_windows.zip
 zip.exe -r %datestring%-simstack_windows.zip run-simstack.bat simstack simstack.desktop simstack.eula tools_win
+
+echo "DONE building."
