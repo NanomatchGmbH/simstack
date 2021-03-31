0b39251e198588a821aa796988a2eb916c3a6025
Timo Strunk <Timo.Strunk@nanomatch.com>
2021-03-31 12:16:17 +0200

    Added sge_smp to choices in settings

--- WaNo/view/WaNoSettings.py
+++ WaNo/view/WaNoSettings.py
@@ -191,6 +191,7 @@ class WaNoRegistrySettings(QWidget):
         qs.addItem("pbs")
         qs.addItem("slurm")
         qs.addItem("lsf")
+        qs.addItem("sge_smp")
         qs.addItem("AiiDA")
         qs.addItem("Internal")
 
