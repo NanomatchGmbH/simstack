ddafed0ee1de7ea95efcfaf12548efb8d5901514
Timo Strunk <Timo.Strunk@nanomatch.com>
2021-12-13 09:32:34 +0100

    Updated release script for simstack

--- release_tools/do_kit_linux_release.sh
+++ release_tools/do_kit_linux_release.sh
@@ -4,7 +4,10 @@ TODAY=$(date "+%F")
 ./simstack_src/release_tools/compiled_client.sh
 if [ -f $TODAY-simstack_linux.tar.gz ]
 then
-    rsync -av $TODAY-simstack_linux.tar.gz nanodev:WordPress_SecureMode_01/nanomatch-files/software/simstack/current_release/
+    #rsync -av $TODAY-simstack_linux.tar.gz nanodev:WordPress_SecureMode_01/nanomatch-files/software/simstack/current_release/
+    rsync -av $TODAY-simstack_linux.tar.gz stack:WordPress_SecureMode_01/nanomatch-files/software/simstack/
+    echo "nanomatch-files/software/simstack/$TODAY-simstack_linux.tar.gz"
+    echo "Please add here: https://www.simstack.de/wp-admin/post.php?post=286&action=edit"
 else
     echo "Could not find $TODAY-simstack_linux.tar.gz. Maybe something went wrong."
 fi
