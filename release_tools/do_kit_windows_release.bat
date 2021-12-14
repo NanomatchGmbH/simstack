aaf6c595d8ad159b0684f0560323a575445399f4
Timo Strunk <Timo.Strunk@nanomatch.com>
2021-12-14 13:07:14 +0100

    Changed SimStack Windows release path

--- release_tools/do_kit_windows_release.bat
+++ release_tools/do_kit_windows_release.bat
@@ -3,5 +3,7 @@ SET datestring=%date:~6,4%-%date:~3,2%-%date:~0,2%
 CALL .\simstack_src\release_tools\compiled_client.bat
 
 echo "DONE BUILDING."
-rsync %datestring%-simstack_windows.zip -avzh -e "ssh -F %USERPROFILE%\.ssh\config -i %USERPROFILE%\.ssh\id_rsa" nanodev:WordPress_SecureMode_01/nanomatch-files/software/simstack/current_release/
+rsync %datestring%-simstack_windows.zip -avzh -e "ssh -F %USERPROFILE%\.ssh\config -i %USERPROFILE%\.ssh\id_rsa" www.simstack.de@ssh.strato.de:WordPress_SecureMode_01/nanomatch-files/software/simstack/
+echo "nanomatch-files/software/simstack/%datestring%-simstack_windows.zip"
+echo "Please add here: https://www.simstack.de/wp-admin/post.php?post=284&action=edit"
 echo "RSYNC done"
