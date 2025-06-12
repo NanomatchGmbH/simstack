59ff1321b83c4dc0588b4ecd56647dda5a536ea2
Timo Strunk <timo.strunk@gmail.com>
2025-06-12 16:36:19 +0200

    removed empty WaNoViews test

--- tests/view/test_WaNoViews.py
+++
@@ -1,3 +0,0 @@
-# This file previously contained tests for WaNoViews classes, but all tests
-# were marked with @pytest.mark.skip and have been removed as requested.
-# The file is left empty but present to maintain the test structure.
